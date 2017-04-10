187e897f42fc5641435067e2e6527108c2ee0d04
Alexander Rusetsky <alex_rusk@mail.ru>
2017-04-10 09:01:13 +0300

    Новые исправления

---
+++ src/Avtocentr297/Controllers/CompanyController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avtocentr297.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult History()
+        {
+            ViewData["Title"] = "История развития компании";
+            return View();
+        }
+
+    }
+}
